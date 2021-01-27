18d876aea997889c3b23165df3af6cef2c15fdd0
Korey Lombardi <koreylombardi17@yahoo.com>
2021-01-27 02:08:33 -0500

    added str()

--- automobile.py
+++ automobile.py
@@ -6,32 +6,39 @@ class Automobile:
 		self.year = year
 		self.gas_mileage = gas_mileage
 
+	def __str__(self):
+		return ("Name: " + self.name 
+			+ "\nYear: " + str(self.year) 
+			+ "\nGas Mileage: " + str(self.gas_mileage))
+
 class Car(Automobile):
 	def __init__(self, name, year, gas_mileage, number_doors):
 		super().__init__(name, year, gas_mileage)
 		self.number_doors = number_doors
+
+	def __str__(self):
+		return (super().__str__() 
+			+ "\nNumber of doors: " + str(self.number_doors))
 
 class Truck(Automobile):
 	def __init__(self, name, year, gas_mileage, bed_type):
 		super().__init__(name, year, gas_mileage)
 		self.bed_type = bed_type
 
+	def __str__(self):
+		return (super().__str__() 
+			+ "\nBed type: " + self.bed_type)
+
+
+# Created Objects of different types
 automobile = Automobile("Automobile", 2008, 28)
 car = Car("Acura TL", 2008, 28, 4)
 truck = Truck("Ford F-150", 2015, 20, "Crew")
 
-print(automobile.name)
-print(automobile.year)
-print(automobile.gas_mileage)
+print(automobile)
 print()
 
-print(car.name)
-print(car.year)
-print(car.gas_mileage)
-print(car.number_doors)
+print(car)
 print()
 
-print(truck.name)
-print(truck.year)
-print(truck.gas_mileage)
-print(truck.bed_type)
+print(truck)
